1de09cc0daa3dafd49cec2ea2151f5c533374ac3
VedantJumle <vedantjumle@gmail.com>
2026-03-25 19:19:27 +0100

    create global submit script

---
+++ scripts/submit_jobs.sh
@@ -0,0 +1,4 @@
+sbatch /scratch/vvjumle/clip-food-good/scripts/job_experiment1.sh
+sbatch /scratch/vvjumle/clip-food-good/scripts/job_experiment2.sh
+sbatch /scratch/vvjumle/clip-food-good/scripts/job_experiment3.sh
+sbatch /scratch/vvjumle/clip-food-good/scripts/job_experiment4.sh
