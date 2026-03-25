f03d94dfc53799f38b82cef9b16b5fee88f9edb1
VedantJumle <vedantjumle@gmail.com>
2026-03-25 22:51:26 +0100

    update runtime for job script

--- scripts/job_experiment2.sh
+++ scripts/job_experiment2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=clip_food_exp2
 #SBATCH --partition=gpu
-#SBATCH --time=01:00:00
+#SBATCH --time=12:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4G
